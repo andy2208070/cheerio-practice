157584197b4ef510229ca60dfa8802cea9e8cd3e
Wits.AndyKuo <Wits.AndyKuo@wistron.com>
2024-03-15 17:55:59 +0800

    test cheer page render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Express' });
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/pages/cheer', function(req, res, next){
+  res.render('pages/cheer-page');
 });
 
 // module.exports = router;
